ead86733f87cd5614c07f141a4e2e81512ce991b
Renato Gava <renatogava2@live.com>
2025-02-27 21:34:26 -0300

    criando a tela de login

---
+++ src/EnglishNow.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnglishNow.Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
